aa798021a220be60a79a941e4be44568ea355b4d
haidirwf <i914900h@gmail.com>
2026-02-09 20:43:31 +0700

    adding vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  base: mode === 'production' ? '/e-library/' : '/',
+  base: '/',
 }));
+
